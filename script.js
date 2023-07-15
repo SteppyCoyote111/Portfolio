dfac0f63867d8aabd7fe2ed794dcf7d34fd3e221
Xus <xusagi.c.y@mailru>
2023-07-16 02:09:17 +0300

    Save

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function openLink() {
-	window.location.href = "file:///G:/Fork/Portfolio_Site/Games/Portfel.html";
-	// window.open("https://xusagi111.github.io/Portfolio/Games/");
-    //window.open("https://www.example.com");
-}
-
 function GoToMainPage() {
-	window.location.href = "file:///G:/Fork/Portfolio_Site/index.html";
+	window.location.href = IS_USING_GLOBAL_LINK == false ? GO_TO_MAIN_PAGE_LOCAL : GO_TO_MAIN_PAGE_GLOBAL;
 	// window.open("https://xusagi111.github.io/Portfolio/");
 }
 
@@ -20,12 +14,12 @@ function GoToMainPage() {
    window.location.href = IS_USING_GLOBAL_LINK == false ? RELEASED_GAMES_LOCAL : RELEASED_GAMES_GLOBAL;
  }
 
-
-
-
 const IS_USING_GLOBAL_LINK = true;
 const RELEASED_GAMES_LOCAL = "file:///G:/Fork/Portfolio_Site/Games/Portfel.html";
 const RELEASED_GAMES_GLOBAL = "https://xusagi111.github.io/Portfolio/Games/Portfel.html";
+
+const GO_TO_MAIN_PAGE_LOCAL = "file:///G:/Fork/Portfolio_Site/index.html";
+const GO_TO_MAIN_PAGE_GLOBAL = "https://xusagi111.github.io/Portfolio/index.html";
 
 
 //Item To Cons Element
