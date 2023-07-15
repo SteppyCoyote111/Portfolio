1f753dcdb17190e3fd63a9118f3add926cf571bf
Xus <xusagi.c.y@mailru>
2023-07-16 01:44:11 +0300

    Save Open Elements

--- script.js
+++ script.js
@@ -21,6 +21,61 @@ function GoToMainPage() {
  }
 
 
-const IS_USING_GLOBAL_LINK = true;
+
+
+const IS_USING_GLOBAL_LINK = false;
 const RELEASED_GAMES_LOCAL = "file:///G:/Fork/Portfolio_Site/Games/Portfel.html";
 const RELEASED_GAMES_GLOBAL = "https://xusagi111.github.io/Portfolio/Games/Portfel.html";
+
+
+
+const items = [
+    { title: 'Элемент 1', description: 'Описание элемента 1' },
+    { title: 'Элемент 2', description: 'Описание элемента 2' },
+    { title: 'Элемент 3', description: 'Описание элемента 3' },
+    { title: 'Элемент 4', description: 'Описание элемента 4' },
+];
+
+let currentIndex = 0;
+
+function showItemDescription(index) {
+    currentIndex = index;
+    const item = items[currentIndex];
+    const itemDescription = document.getElementById('item-description');
+    itemDescription.innerHTML = `
+        <h2>${item.title}</h2>
+        <p>${item.description}</p>
+        <div class="navigation-buttons">
+            <button onclick="prevItem()">Назад</button>
+            <button onclick="nextItem()">Вперед</button>
+        </div>
+    `;
+    itemDescription.style.display = 'block';
+
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
+}
+
+function hideItemDescription() {
+    const itemDescription = document.getElementById('item-description');
+    itemDescription.style.display = 'none';
+
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
+
+function prevItem() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = items.length - 1;
+    }
+    showItemDescription(currentIndex);
+}
+
+function nextItem() {
+    currentIndex++;
+    if (currentIndex >= items.length) {
+        currentIndex = 0;
+    }
+    showItemDescription(currentIndex);
+}
